e2df3916b8cd386639554a23ea7827ce91a9ea77
Anas Ismail <100934197+AnasIsmail8996@users.noreply.github.com>
2026-05-17 20:52:31 +0500

    api.js file

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_BASE_URI || "http://localhost:3000/api";
-
+const API_BASE_URL = import.meta.env.VITE_BASE_URI || "http://localhost:5000/api";
+console.log("🚀 API Base URL:", API_BASE_URL);
 axios.defaults.withCredentials = true;
 
 const apiClient = axios.create({
